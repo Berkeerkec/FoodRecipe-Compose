f578a8de2dd1ae2521dce20b8982d0bde1df7c64
Berke Erkeç <berkeerkec0@gmail.com>
2023-08-19 14:21:23 +0300

    Remote repositories have been created.

---
+++ app/src/main/java/com/berkeerkec/foodrecipescompose/data/repository/RemoteDataSourceImpl.kt
@@ -0,0 +1,15 @@
+package com.berkeerkec.foodrecipescompose.data.repository
+
+import com.berkeerkec.foodrecipescompose.data.remote.FoodRecipesApi
+import com.berkeerkec.foodrecipescompose.data.remote.dto.FoodRecipe
+import com.berkeerkec.foodrecipescompose.domain.repository.RemoteDataSource
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSourceImpl @Inject constructor(
+    private val api : FoodRecipesApi
+) : RemoteDataSource{
+    override suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return api.getRecipes(queries)
+    }
+}
---
+++ app/src/main/java/com/berkeerkec/foodrecipescompose/domain/repository/RemoteDataSource.kt
@@ -0,0 +1,9 @@
+package com.berkeerkec.foodrecipescompose.domain.repository
+
+import com.berkeerkec.foodrecipescompose.data.remote.dto.FoodRecipe
+import retrofit2.Response
+
+interface RemoteDataSource {
+
+    suspend fun getRecipes(queries : Map<String,String>) : Response<FoodRecipe>
+}
